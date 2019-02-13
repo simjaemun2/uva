d9986e1fa04ab68038d9d55a43ecae4d0a2c6187
1 <a1@1ui-MacBookPro.local>
2019-02-13 21:45:36 +0900

    LIS with upper_bound

---
+++ ch 3/3.5/11368.cpp
@@ -0,0 +1,72 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+typedef long long int LL;
+
+const int MAX_N = 10000;
+
+int tc, TC;
+int N;
+
+bool compare(const pair<int, int>& a, const pair<int, int>& b)
+{
+	if (a.first == b.first)
+		return a.second < b.second;
+	return a.first > b.first;
+}
+
+int main(int argc, char** argv)
+{
+	std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+	
+	cin >> TC;
+
+	for (tc = 1; tc <= TC; tc++)
+	{
+		cin >> N;
+		vector<pair<int, int> > v(N);
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> v[n].first >> v[n].second;
+		}
+
+		sort(v.begin(), v.end(), compare);
+
+		vector<int> lis(1, -1);
+
+		for (auto p : v)
+		{
+			if (lis.back() <= p.second)
+			{
+				lis.push_back(p.second);
+			}
+			else
+			{
+				*upper_bound(lis.begin(), lis.end(), p.second) = p.second;
+			}
+		}
+
+		cout << lis.size() - 1 << '\n';
+	}
+
+
+	return 0;
+}
+
