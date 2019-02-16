4181b64279f84f7e58dd2e6b3c9cea9639701e6b
1 <a1@1ui-MacBookPro.local>
2019-02-16 10:43:13 +0900

    LIS, LDS with lower_bound, weighted LIS, LDS

---
+++ ch 3/3.5/11456.cpp
@@ -0,0 +1,86 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+typedef long long int LL;
+
+const int MAX_N = 10000;
+
+int tc, TC;
+int N;
+int input[MAX_N];
+
+int main(int argc, char** argv)
+{
+	std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+	
+	cin >> TC;
+
+	for (tc = 1; tc <= TC; tc++)
+	{
+		cin >> N;
+		
+		if (!N)
+		{
+			cout << "0\n";
+			continue;
+		}
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> input[n];
+		}
+
+		vector<int> lis(1, -1);
+		vector<int> lds(1, -987654321);
+		vector<int> answer(N);
+
+		for (int n = N-1; n >= 0; n--)
+		{
+			if (lis.back() < input[n])
+			{
+				lis.push_back(input[n]);
+				answer[n] += lis.size() - 1;
+			}
+			else
+			{
+				auto it = lower_bound(lis.begin(), lis.end(), input[n]);
+				*it = input[n];
+				answer[n] += it - lis.begin();
+			}
+
+			if (lds.back() < -input[n])
+			{
+				lds.push_back(-input[n]);
+				answer[n] += lds.size() - 1;
+			}
+			else
+			{
+				auto it = lower_bound(lds.begin(), lds.end(), -input[n]);
+				*it = -input[n];
+				answer[n] += it - lds.begin();
+			}
+		}
+
+		cout << *max_element(answer.begin(), answer.end()) -1 << '\n';		
+	}
+
+
+	return 0;
+}
+
---
+++ ch 3/3.5/11790.cpp
@@ -0,0 +1,107 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+typedef long long int LL;
+
+const int MAX_N = 10000;
+
+int tc, TC;
+int N;
+
+int my_lower_bound(vector<pair<int, int> >& v, int key)
+{
+	int left = 0, right = v.size();
+
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		
+		if (v[mid].first < key)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid;
+		}
+	}
+
+	return right;
+}
+
+int main(int argc, char** argv)
+{
+	std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+	
+	cin >> TC;
+
+	for (tc = 1; tc <= TC; tc++)
+	{
+		cin >> N;
+
+		vector<int> H(N), W(N), LIS(N), LDS(N);
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> H[n];
+		}
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> W[n];
+			LIS[n] = LDS[n] = W[n];
+		}
+		
+		for (int i = 1; i < N; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				if (H[i] > H[j])
+				{
+					LIS[i] = max(LIS[i], LIS[j] + W[i]);
+				}
+
+				if (H[i] < H[j])
+				{
+					LDS[i] = max(LDS[i], LDS[j] + W[i]);
+				}
+			}
+		}
+
+		int max_lis = *max_element(begin(LIS), end(LIS));
+		int max_lds = *max_element(begin(LDS), end(LDS));
+
+
+		cout << "Case " << tc << ". ";
+		
+		if (max_lis >= max_lds)
+		{
+			cout << "Increasing (" << max_lis << "). Decreasing (" << max_lds << ").\n";
+		}
+		else
+		{
+			cout << "Decreasing (" << max_lds << "). Increasing (" << max_lis << ").\n";
+		}
+		
+	}
+
+
+	return 0;
+}
+
