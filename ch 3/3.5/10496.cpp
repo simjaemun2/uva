5ba862048cdb14fbc7838bb20f90a4122bd36c08
1 <a1@1ui-MacBookPro.local>
2019-02-24 10:35:29 +0900

    TSP with bitmask DP

---
+++ ch 3/3.5/10496.cpp
@@ -0,0 +1,95 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef pair<int, int > II;
+
+const int MAX_N = 10;
+
+int tc, TC;
+int SX, SY;
+int N;
+int X[MAX_N];
+int Y[MAX_N];
+
+int cache[MAX_N][1 << MAX_N];
+
+int getDistance(int sx, int sy, int dx, int dy)
+{
+	return abs(sx - dx) + abs(sy - dy);
+}
+
+int dp(int pos, int mask)
+{
+	if (mask == (1 << N) - 1)
+	{
+		return getDistance(pos[X], pos[Y], SX, SY);
+	}
+
+	int& ret = cache[pos][mask];
+
+	if (ret != -1)
+		return ret;
+
+	ret = 987654321;
+
+	for (int n = 0; n < N; n++)
+	{
+		if (!(mask & (1 << n)))
+		{
+			ret = min(ret, getDistance(X[pos],Y[pos],X[n],Y[n]) + dp(n, mask | (1 << n)));
+		}
+	}
+
+	return ret;
+}
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+	
+	cin >> TC;
+	
+	for (tc = 1; tc <= TC; tc++)
+	{
+		cin >> SX >> SY >> SX >> SY >> N;
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> X[n] >> Y[n];
+		}
+
+		memset(cache, -1, sizeof cache);
+
+		int answer = 987654321;
+
+		for (int n = 0; n < N; n++)
+		{
+			answer = min(answer, dp(n, 1 << n) + getDistance(X[n], Y[n], SX, SY));
+		}
+
+		cout << "The shortest path has length " << answer << '\n';
+	}
+	
+	return 0;
+}
+
---
+++ ch 3/3.5/11284.cpp
@@ -0,0 +1,150 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef pair<int, int > II;
+const double INF = 9876543212345;
+
+
+const int MAX_N = 50;
+const int MAX_M = 1000;
+const int MAX_P = 13;
+
+int tc, TC;
+int N, M, P;
+LL dist[MAX_N + 1][MAX_N + 1];
+pair<int, LL> operas[MAX_P + 1];
+
+LL cache[MAX_P][1 << MAX_P];
+
+int getDistance(int sx, int sy, int dx, int dy)
+{
+	return abs(sx - dx) + abs(sy - dy);
+}
+
+LL dp(int p, int mask, LL sum)
+{
+	if (mask == (1 << (P+1)))
+	{
+		return dist[operas[p].first][0];
+	}
+
+	LL& ret = cache[p][mask];
+
+	if (ret != -1)
+		return ret;
+
+	ret = dist[operas[p].first][0] + sum;
+
+	for (int np=1; np <= P; np++)
+	{
+		if (!((1 << np) & mask))
+		{
+			ret = min(ret,
+				dp(np, (1 << np) | mask, sum - operas[np].second) + dist[operas[p].first][operas[np].first]
+			);
+		}
+	}
+
+	return ret;
+}
+
+void FW()
+{
+	for (int k = 0; k < N; k++)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+			}
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+	
+	cin >> TC;
+	
+	for (tc = 1; tc <= TC; tc++)
+	{
+		memset(dist, 0, sizeof dist);
+
+		cin >> N >> M;
+
+		N++;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				dist[i][j] = INF;
+			}
+		}
+
+		for (int m = 0; m < M; m++)
+		{
+			int a, b,c,d;
+			scanf("%d %d %d.%d", &a, &b, &c, &d);
+			
+			dist[a][b] = min(dist[a][b], (LL)(c * 100 + d));
+			dist[b][a] = min(dist[b][a], (LL)(c * 100 + d));
+		}
+
+		FW();
+
+		cin >> P;
+		
+		LL sum = 0;
+		for (int p = 1; p <= P; p++)
+		{
+			int c, d;
+
+			scanf("%d %d.%d", &operas[p].first, &c, &d);
+
+			operas[p].second = (LL)(c*100 + d);
+			sum += operas[p].second;
+		}
+
+		LL answer = INF;
+		memset(cache, -1, sizeof cache);
+
+		dist[0][0] = 0;
+		
+		answer = min(answer, dp(0, 1, sum));
+
+		if (sum <= answer)
+		{
+			cout << "Don't leave the house\n";
+		}
+		else
+		{
+			printf("Daniel can save $%.2lf\n", (double)(sum - answer) / 100.0);
+		}
+	}
+	
+	return 0;
+}
+
