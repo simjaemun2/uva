6d0640efaa15365a8a5195a302d0bb810da4cfad
1 <a1@1ui-MacBookPro.local>
2019-03-02 13:05:30 +0900

    DP

---
+++ ch 3/3.5/10910.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > II;
+
+const int INF = 2000000000;
+const int MAX_NPT = 70;
+
+
+int tc, TC;
+int N, T, P;
+int cache[MAX_NPT*2][MAX_NPT*2];
+
+
+int dp(int n, int t)
+{
+	if (t == 0 || t == n)
+		return 1;
+
+	int& ret = cache[n][t];
+
+
+	if (ret != -1)
+		return ret;
+
+	return ret = dp(n - 1, t - 1) + dp(n - 1, t);
+}
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	memset(cache, -1, sizeof cache);
+		
+	cin >> TC;
+
+	for (tc = 1; tc <= TC; tc++)
+	{
+		cin >> N >> T >> P;
+
+		T -= N * P;
+
+		if (T < 0)
+			cout << "0\n";
+		else
+			cout << dp(N + T - 1, N - 1) << '\n';
+	}
+	
+	
+	return 0;
+}
+
---
+++ ch 3/3.5/10980.cpp
@@ -0,0 +1,99 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > II;
+
+const int INF = 2000000000;
+const int MAX_N = 100;
+const int MAX_M = 20;
+const int MAX_K = 100;
+
+int tc;
+
+int M;
+double unit;
+string line;
+
+int N[MAX_M+1];
+double P[MAX_M+1];
+int K[MAX_K+1];
+
+double cache[MAX_K+1];
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+	
+	while (getline(cin, line))
+	{
+		cout << "Case " << ++tc << ":\n";
+
+		istringstream(line) >> P[0] >> M;
+
+		for (int m = 1; m <= M; m++)
+		{
+			getline(cin, line);
+			istringstream(line) >> N[m] >> P[m];
+		}
+
+		getline(cin, line);
+		istringstream iss(line);
+		int nk = 0;
+		int max_k = 0;
+
+		
+		while (iss >> K[nk])
+		{
+			max_k = max(max_k, K[nk++]);
+		}
+
+		for (int k = 1; k <= max_k; k++)
+		{
+			cache[k] = P[0] * k;
+		}
+
+		for (int i = 1; i <= M; i++)
+		{
+			for (int k = 1; k < N[i]; k++)
+			{
+				cache[k] = min(cache[k], P[i]);
+			}
+
+			for (int k = N[i]; k <= max_k; k++)
+			{
+				cache[k] = min(cache[k], cache[k - N[i]] + P[i]);
+			}
+		}
+
+		for (int k = 0; k < nk; k++)
+		{
+			printf("Buy %d for $%.2f\n", K[k], cache[K[k]]);
+		}
+	}
+
+	
+
+	return 0;
+}
+
