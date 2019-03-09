4b343cb71e77b2b5272fc6a4fb8a08eb221f21a5
simjaemun2 <simjaemun2@naver.com>
2019-03-09 22:00:42 +0900

    greedy: interval covering

---
+++ ch3/3.4/uva01193.cpp
@@ -0,0 +1,97 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > II;
+typedef pair<double, double > DD;
+
+const int INF = 2000000000;
+const int MAX_N = 10000;
+
+
+int N;
+double D;
+int tc;
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	while (cin >> N >> D, N)
+	{
+		vector<DD > V;
+		bool isPossible = true;
+
+		for (int n = 0; n < N; n++)
+		{
+			double x, y;
+			cin >> x >> y;
+			
+			if (y > D)
+			{
+				isPossible = false;
+			}
+			
+			double dx = sqrt(D*D - y * y);
+			
+			V.push_back({ x - dx, x + dx });
+		}
+
+		int answer = -1;
+
+		if (isPossible)
+		{
+			sort(V.begin(), V.end());
+
+			answer = 0;
+
+			for (int i = 0,j; i < N; i++)
+			{
+				answer++;
+				for (j = i + 1; j < N; j++)
+				{
+					if (V[i].second >= V[j].first)
+					{
+						if (V[i].second >= V[j].second)
+						{
+							i = j;
+						}	
+					}
+					else
+					{
+						i = j - 1;
+						break;
+					}
+				}
+
+				if (j == N)
+					break;
+			}
+		}
+
+		cout << "Case " << ++tc << ": " << answer << '\n';
+	}
+
+
+	return 0;
+}
---
+++ ch3/3.4/uva10382.cpp
@@ -0,0 +1,91 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > II;
+
+const int INF = 2000000000;
+const int MAX_N = 10000;
+
+
+int N;
+double L, W;
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	while (cin >> N >> L >> W)
+	{
+		W /= 2.0;
+
+		vector<pair<double, double> > v;
+
+		for (int n = 0; n < N; n++)
+		{
+			double c, r;
+			cin >> c >> r;
+
+			if (r > W)
+			{
+				double dx = sqrt(r*r - W * W);
+				v.push_back({ c - dx, c + dx });
+			}
+		}
+
+		sort(v.begin(), v.end());
+
+		double right = 0.0;
+		int answer = 0;
+
+		for (int i = 0, j; i < v.size(); i = j)
+		{
+			if (v[i].first > right)
+				break;
+
+			for (j = i + 1; j < v.size() && v[j].first <= right; j++)
+			{
+				if (v[j].second > v[i].second)
+				{
+					i = j;
+				}
+			}
+
+			answer++;
+
+			right = v[i].second;
+
+			if (right >= L)
+				break;
+		}
+		
+
+		if (right < L)
+			answer = -1;
+
+		cout << answer << '\n';
+	}
+
+
+	return 0;
+}
