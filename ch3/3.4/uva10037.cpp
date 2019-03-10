493509aaaae7e34b3e07281fb7d7476aa40a69f0
simjaemun2 <simjaemun2@naver.com>
2019-03-10 18:43:16 +0900

    greedy

---
+++ ch3/3.4/uva10037.cpp
@@ -0,0 +1,116 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+
+const int INF = 2000000000;
+const int MAX_M = 5000;
+
+int tc, TC;
+int N;
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	cin >> TC;
+
+	for(tc=1; tc<=TC; tc++)
+	{
+		if (tc > 1)
+			cout << '\n';
+		cin >> N;
+
+		vector<int> v(N+1);
+		v[0] = 0;
+		for (int n = 1; n <= N; n++)
+		{
+			cin >> v[n];
+		}
+
+		sort(v.begin(), v.end());
+
+		int tail = N;
+		int sum = 0;
+		stringstream iss;
+		while (tail >= 4)
+		{
+			int c1 = v[2]
+				+ v[1]
+				+ v[tail]
+				+ v[2];
+			
+			int c2 = v[tail]
+				+ v[1]
+				+ v[tail - 1]
+				+ v[1];
+
+			if (c1 < c2)
+			{
+				sum += c1;
+				iss << v[1] << ' ' << v[2] << '\n'
+					<< v[1] << '\n'
+					<< v[tail - 1] << ' ' << v[tail] << '\n'
+					<< v[2] << '\n';
+			}
+			else
+			{
+				sum += c2;
+				iss << v[1] << ' ' << v[tail] << '\n'
+					<< v[1] << '\n'
+					<< v[1] << ' ' << v[tail - 1] << '\n'
+					<< v[1] << '\n';
+			}
+
+			tail -= 2;
+		}
+
+		if (tail == 3)
+		{
+			sum += v[3] + v[1] + v[2];
+			iss << v[1] << ' ' << v[3] << '\n'
+				<< v[1] << '\n'
+				<< v[1] << ' ' << v[2] << '\n';
+		}
+		else if (tail == 2)
+		{
+			sum += v[2];
+			iss << v[1] << ' ' << v[2] << '\n';
+		}
+		else // tail == 1
+		{
+			sum += v[1];
+			iss << v[1] << '\n';
+		}
+
+		cout << sum << '\n' << iss.str();
+	}
+	
+	
+
+
+	return 0;
+}
---
+++ ch3/3.4/uva10249.cpp
@@ -0,0 +1,125 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+#include <map>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+
+const int INF = 2000000000;
+const int MAX_M = 5000;
+
+int tc, TC;
+int N, M;
+
+struct Node{
+	int idx, size;
+	vector<int> v;
+};
+
+bool compare(const PII& a, const PII& b)
+{
+	if (a.first == b.first)
+		return a.second < b.second;
+	return a.first > b.first;
+}
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	while (cin >> M >> N, M)
+	{
+		vector<PII> vn(N), vm(M);
+		for (int m = 0; m < M; m++)
+		{
+			cin >> vm[m].first;
+			vm[m].second = m + 1;
+		}
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> vn[n].first;
+			vn[n].second = n + 1;
+		}
+
+		sort(vm.begin(), vm.end(), compare);
+		sort(vn.begin(), vn.end());
+
+		map<int, vector<int> > mv;
+		
+		bool isPossible = true;
+
+
+		int start = 0;
+		for (int m = 0; m < M; m++)
+		{
+			if (N - start < vm[m].first)
+			{
+				isPossible = false;
+				break;
+			}
+
+			int acc = 0;
+
+			for (int i = 0; i < vm[m].first; i++)
+			{
+				mv[vm[m].second].push_back(vn[start + i].second);
+				vn[start + i].first--;
+				if (!vn[start + i].first)
+				{
+					acc++;
+				}
+			}
+
+			start += acc;
+		}
+		
+		if (!isPossible)
+		{
+			cout << "0\n";
+		}
+		else
+		{
+			cout << "1\n";
+
+			for (int i = 1; i <= M; i++)
+			{
+				sort(mv[i].begin(), mv[i].end());
+
+				for (int a : mv[i])
+				{
+					cout << a << ' ';
+				}
+
+				cout << '\n';
+			}
+		}
+	}
+	
+	
+
+
+	return 0;
+}
