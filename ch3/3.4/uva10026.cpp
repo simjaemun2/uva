58060f376ae606237076d24f067489799be0fd28
simjaemun2 <simjaemun2@naver.com>
2019-03-10 14:19:30 +0900

    greedy

---
+++ ch3/3.4/uva10026.cpp
@@ -0,0 +1,89 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+
+const int INF = 2000000000;
+const int MAX_M = 5000;
+
+int tc, TC;
+int N;
+
+struct Node {
+	double ts;
+	int idx;
+};
+
+bool compare(const Node& a, const Node& b)
+{
+	if (a.ts == b.ts)
+	{
+		return a.idx < b.idx;
+	}
+
+	return a.ts > b.ts;
+}
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	cin >> TC;
+
+	for(tc=1; tc<=TC; tc++)
+	{
+		if (tc > 1)
+			cout << '\n';
+
+		cin >> N;
+
+		vector<Node> v(N);
+
+		for (int n = 0; n < N; n++)
+		{
+			double t, s;
+			cin >> t >> s;
+			v[n].ts = s / t;
+			v[n].idx = n + 1;
+		}
+
+		sort(v.begin(), v.end(), compare);
+
+		cout << v[0].idx;
+
+		for (int i=1; i<v.size(); i++)
+		{
+			cout << ' ' << v[i].idx;
+		}
+
+		cout << '\n';
+	}
+	
+	
+
+
+	return 0;
+}
---
+++ ch3/3.4/uva12321.cpp
@@ -0,0 +1,93 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+
+const int INF = 2000000000;
+const int MAX_M = 5000;
+
+int tc, TC;
+int L, G;
+
+
+
+int main(int argc, char** argv)
+{
+	//std::ios::sync_with_stdio(false);
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	while (cin >> L >> G, L+G)
+	{
+		vector<PII> v;
+
+		for (int g = 0; g < G; g++)
+		{
+			int x, r;
+			cin >> x >> r;
+			v.push_back({ x - r, x + r });
+		}
+
+		sort(v.begin(), v.end());
+
+		int answer = 0;
+		int right = 0;
+
+		for (int i = 0, j; i < v.size(); i++)
+		{
+			if (v[i].first > right)
+			{
+				answer = -1;
+				break;
+			}
+
+			for (int j = i + 1; j < v.size() && v[j].first <= right; j++)
+			{
+				if (v[j].second > v[i].second)
+				{
+					i = j;
+				}
+			}
+
+			answer++;
+			right = v[i].second;
+
+			if (right >= L)
+				break;
+		}
+
+		if (right < L)
+			answer = -1;
+
+		if (answer == -1)
+			cout << "-1\n";
+		else
+			cout << G - answer << '\n';
+	}
+	
+	
+
+
+	return 0;
+}
