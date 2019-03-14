b77886acb7ffb019475c8b0e405d6e8d315fb44c
simjaemun2 <simjaemun2@naver.com>
2019-03-14 21:42:10 +0900

    greedy with sort

---
+++ ch3/3.4/uva10340.cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+#include <map>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+
+const int INF = 2000000000;
+const int MAX_N = 20000;
+
+int TC;
+int N;
+
+string S, T;
+
+
+int main(int argc, char** argv)
+{
+
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	std::ios::sync_with_stdio(false);
+
+	while (cin >> S >> T)
+	{
+		int s = 0, t = 0;
+
+		while (s < S.size() && t < T.size())
+		{
+			if (S[s] == T[t])
+				s++;
+			t++;
+		}
+
+
+		if (s == S.size())
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+	}
+
+	return 0;
+
+}
---
+++ ch3/3.4/uva11900.cpp
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+#include <map>
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+
+const int INF = 2000000000;
+const int MAX_N = 20000;
+
+int tc, TC;
+int N,P,Q;
+
+
+int main(int argc, char** argv)
+{
+#ifdef _WIN32
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	cin >> TC;
+
+	for (tc = 1; tc <= TC; tc++)
+	{
+		cin >> N >> P >> Q;
+
+		vector<int> v(N);
+
+		int gm = 0;
+		int num = 0;
+		int g;
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> g;
+
+			if (num < P && gm + g <= Q)
+			{
+				gm += g;
+				num++;
+			}
+		}
+
+		cout << "Case " << tc << ": " << num << '\n';
+	}
+
+	return 0;
+
+}
