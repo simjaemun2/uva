7fd28984783cd192ca9566ed35d257390538a166
simjaemun2 <simjaemun2@naver.com>
2019-06-22 19:21:24 +0900

    dijkstra

---
+++ ch4/4.4/uva10166.cpp
@@ -0,0 +1,194 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+#include <map>
+#include <limits.h>
+#include <queue>
+
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+typedef pair<int, PII> PPII;
+typedef vector<int > VI;
+typedef vector<VI > VVI;
+typedef vector<PII > VPII;
+typedef vector<string > VS;
+typedef vector<bool > VB;
+typedef vector<double > VD;
+typedef vector<PII> VPII;
+typedef vector<vector<PII> > VVPII;
+
+const int INF = 987654321;
+int tc, TC;
+
+const int MAX_D = 4;
+const int MAX_DIAG = 8;
+//            S N W E
+//            0 1 2 3
+int DY[] = { -1,1,0,0 , -1,-1,1,1};
+int DX[] = { 0,0,-1,1 , -1,1,-1,1};
+const int SOUTH = 0;
+const int NORTH = 1;
+const int WEST = 2;
+const int EAST = 3;
+
+int N, T, M;
+
+struct Vertex {
+	int start;
+	int end;
+	int station;
+	int orgStart;
+};
+
+bool operator<(Vertex a, Vertex b)
+{
+	return a.end < b.end;
+}
+
+int main(int argc, char** argv)
+{
+#ifdef _WIN32
+	freopen("Text1.txt", "r", stdin);
+#endif
+	int tc = 1;
+	while (cin >> N, N)
+	{
+		map<string, int> m;
+		vector<vector<Vertex > > adj(N);
+		map<PII, int> dist;
+
+		for (int n = 0; n < N; n++)
+		{
+			string s;
+			cin >> s;
+			m[s] = n;
+		}
+
+		cin >> T;
+		while (T--)
+		{
+			cin >> M;
+
+			string pStation, pStart;
+			cin >> pStart >> pStation;
+
+			int pStationIndex = m[pStation];
+			int ps = stoi(pStart);
+
+			dist[{pStationIndex, ps}] = -1;
+
+			while (--M)
+			{
+				string start, station;
+
+				cin >> start >> station;
+
+				int st = stoi(start);
+
+				dist[{m[station], st}] = -1;
+
+				Vertex vertex;
+				vertex.start = ps;
+				vertex.end = st;
+				vertex.station = m[station];
+				adj[pStationIndex].push_back(vertex);
+
+				ps = st;
+				pStationIndex = m[station];
+			}
+		}
+
+		string st, s, e;
+		cin >> st >> s >> e;
+		int t = stoi(st);
+		int si = m[s];
+		int ei = m[e];
+
+		priority_queue<Vertex> pq;
+		
+		Vertex sv;
+		sv.end = t;
+		sv.orgStart = 0;
+		sv.station = si;
+
+		pq.push(sv);
+
+		int aStart = INF;
+		int aEnd = INF;
+
+		while (!pq.empty())
+		{
+			int cStation = pq.top().station;
+			int cEnd = pq.top().end;
+			int cOrgStart = pq.top().orgStart;
+
+			pq.pop();
+
+			if (cStation == ei)
+			{
+				if (aEnd > cEnd || aEnd == cEnd && aStart < cOrgStart)
+				{
+					aEnd = cEnd;
+					aStart = cOrgStart;
+				}
+
+				continue;
+			}
+
+			for (Vertex candi : adj[cStation])
+			{
+				if (cEnd <= candi.start && (!cOrgStart || dist[{candi.station, candi.start}] < cOrgStart))
+				{
+					Vertex nv;
+					nv.end = candi.end;
+
+					if (cOrgStart)
+					{
+						nv.orgStart = cOrgStart;
+					}
+					else
+					{
+						nv.orgStart = candi.start;
+					}
+					
+					nv.station = candi.station;
+					pq.push(nv);
+					dist[{candi.station, candi.start}] = nv.orgStart;
+				}
+			}
+		}
+
+		if (aStart == INF)
+		{
+			cout << "No connection\n";
+		}
+		else
+		{
+			printf("%02d%02d %02d%02d\n", aStart / 100, aStart % 100, aEnd / 100, aEnd % 100);
+		}
+
+		tc++;
+	}
+
+	return 0;
+}
---
+++ ch4/4.4/uva10967.cpp
@@ -0,0 +1,174 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+#include <set>
+#include <tuple>
+#include <stack>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <functional>
+#include <map>
+#include <limits.h>
+#include <queue>
+
+
+using namespace std;
+
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef pair<int, int > PII;
+typedef pair<double, double > PDD;
+typedef pair<LL, LL> PLL;
+typedef pair<int, PII> PPII;
+typedef vector<int > VI;
+typedef vector<VI > VVI;
+typedef vector<PII > VPII;
+typedef vector<string > VS;
+typedef vector<bool > VB;
+typedef vector<double > VD;
+typedef vector<PII> VPII;
+typedef vector<vector<PII> > VVPII;
+
+const int INF = 987654321;
+int tc, TC;
+
+const int MAX_D = 4;
+const int MAX_DIAG = 8;
+//            N   W  S  E
+//            0   1  2  3
+int DY[] = { -1,  0, 1, 0 , -1,-1,1,1};
+int DX[] = {  0, -1, 0, 1 , -1,1,-1,1};
+
+const int NORTH = 0;
+const int WEST = 1;
+const int SOUTH = 2;
+const int EAST = 3;
+
+int N, M, S;
+
+const int MAX_N = 100;
+
+int doors[MAX_N][MAX_N];
+char board[MAX_N][MAX_N + 1];
+char w2i[128];
+
+
+int main(int argc, char** argv)
+{
+#ifdef _WIN32
+	freopen("Text1.txt", "r", stdin);
+#endif
+	
+	w2i['N'] = 0;
+	w2i['W'] = 1;
+	w2i['S'] = 2;
+	w2i['E'] = 3;
+
+	cin >> TC;
+	for (tc = 1; tc <= TC; tc++)
+	{
+		VPII vd;
+
+		cin >> N >> M;
+
+		VVI dist(N, VI(M, INF));
+
+		for (int n = 0; n < N; n++)
+		{
+			cin >> board[n];
+			
+			for (int m = 0; m < M; m++)
+			{
+				if (board[n][m] != '.' && board[n][m] != '#')
+				{
+					vd.push_back({ n, m });
+				}
+			}
+		}
+
+		for (auto p : vd)
+		{
+			cin >> S;
+			doors[p.first][p.second] = S;
+		}
+		
+
+		priority_queue<pair<int, PII > > pq;
+		pq.push({ 0, {N - 1, 0} });
+		dist[N - 1][0] = 0;
+
+		while (!pq.empty())
+		{
+			int cy = pq.top().second.first;
+			int cx = pq.top().second.second;
+			int cd = -pq.top().first;
+
+			pq.pop();
+
+			if (cy == 0 && cx == M - 1)
+			{
+				continue;
+			}
+
+			for (int d = 0; d < MAX_D; d++)
+			{
+				int ny = cy + DY[d];
+				int nx = cx + DX[d];
+
+				if (0 <= ny && ny < N
+					&& 0 <= nx && nx < M)
+				{
+					int step = 0;
+						
+					if (board[cy][cx] != '.')
+					{
+						step = abs(w2i[board[cy][cx]] - d) % 2 ? doors[cy][cx] : 2 * doors[cy][cx];
+					}
+						
+					if (board[ny][nx] == '.')
+					{
+						int nd = cd + 1 + step;
+
+						if (dist[ny][nx] > nd)
+						{
+							dist[ny][nx] = nd;
+							pq.push({ -nd, {ny, nx} });
+						}
+					}
+					else if (board[ny][nx] != '#')
+					{
+						if ((d + 2) % MAX_D == w2i[board[ny][nx]])
+						{
+							int nd = cd + 1 + step;
+
+							if (dist[ny][nx] > nd)
+							{
+								dist[ny][nx] = nd;
+								pq.push({ -nd, {ny, nx} });
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if (dist[0][M - 1] == INF)
+		{
+			cout << "Poor Kianoosh\n";
+		}
+		else
+		{
+			cout << dist[0][M - 1] << '\n';
+		}
+	}
+
+	return 0;
+}
